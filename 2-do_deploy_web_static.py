211a1c9287797b873223bbb1bf53e22ecbe57cc8
Cipher Phantom <oyepitan10@gmail.com>
2023-10-05 09:12:51 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python3
 """Defines the do_deploy function"""
 import os
-from fabric.api import put, run, env
+import datetime
+from fabric.api import put, run, env, local
 
 
 env.hosts = ["100.26.18.88", "54.197.46.69"]
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+    date = datetime.datetime.now()
+    tgz_file_path = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            date.year,
+            date.month,
+            date.day,
+            date.hour,
+            date.minute,
+            date.second
+            )
+    result = local("tar -cvzf {} web_static".format(tgz_file_path))
+    if result.failed:
+        return None
+    return tgz_file_path
 
 
 def do_deploy(archive_path):
