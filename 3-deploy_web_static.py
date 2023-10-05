6fb546e10b648a96caa447a0a3b8dd52e56613e8
Cipher Phantom <oyepitan10@gmail.com>
2023-10-05 09:04:54 +0100

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Defines functions for deployment"""
 do_pack = __import__("1-pack_web_static").do_pack
 do_deploy = __import__("2-do_deploy_web_static").do_deploy
