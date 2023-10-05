d307d1b8746066d63e891c4ba49c0dbcabd37edd
Cipher Phantom <oyepitan10@gmail.com>
2023-10-05 23:15:27 +0100

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,28 @@
 # sets up your web servers for the deployment of web_static.
 
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    
+	add_header X-Served-By ${hostname};
+    
+	root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      internal;
+    }
+}"
+
 exec { 'apt update':
   command => '/usr/bin/apt-get update',
 }
@@ -21,34 +44,38 @@ file { '/data/web_static/shared/':
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body></html>",
+  content => "Holberton School\n",
 }
 
-exec { 'rm link':
-  command => 'rm -f /data/web_static/current',
-}
 
 file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test/',
-  require => Exec['rm link'],
+  ensure => 'link',
+  target => '/data/web_static/releases/test/',
 }
 
 exec { 'ubuntu':
   command => 'chown -R ubuntu:ubuntu /data/',
 }
 
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School\n",
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n",
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf,
+  require => Package['nginx'],
+}
+
 service {'nginx':
   ensure => 'running',
   enable => true,
-}
-
-file_line { 'nginx config':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  match    => '^}$',
-  line     => "\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n}",
-  multiple => false,
-  require  => Package['nginx'],
-  notify   => Service['nginx'],
 }
