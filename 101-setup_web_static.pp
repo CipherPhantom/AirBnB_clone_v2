03d13ea654eb02df79f51d26e4ff828947456a6a
Cipher Phantom <oyepitan10@gmail.com>
2023-10-05 23:48:41 +0100

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,16 +1,16 @@
-# sets up your web servers for the deployment of web_static.
+# Configures a web server for deployment of web_static.
 
+# Nginx configuration file
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    
-	add_header X-Served-By ${hostname};
-    
-	root   /var/www/html;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
     index  index.html index.htm;
 
     location /hbnb_static {
         alias /data/web_static/current;
+        index index.html index.htm;
     }
 
     location /redirect_me {
@@ -19,81 +19,73 @@ $nginx_conf = "server {
 
     error_page 404 /404.html;
     location /404 {
+      root /var/www/html;
       internal;
     }
 }"
 
-exec { 'apt update':
-  command => '/usr/bin/apt-get update',
-}
-
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt update'],
-}
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
 file { '/data':
-  ensure  => 'directory',
-}
+  ensure  => 'directory'
+} ->
 
 file { '/data/web_static':
-  ensure  => 'directory',
-  require => File['/data'],
-}
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases':
-  ensure  => 'directory',
-  require => File['/data/web_static'],
-}
-
-file { '/data/web_static/shared':
-  ensure  => 'directory',
-  require => File['/data/web_static'],
-}
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test':
-  ensure  => 'directory',
-  require => File['/data/web_static/releases'],
-}
+  ensure => 'directory'
+} ->
 
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School\n",
-  require => File['/data/web_static/releases/test'],
-}
-
+  content => "Holberton School Puppet\n"
+} ->
 
 file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test/',
-  require => File['/data/web_static/releases/test'],
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-exec { 'ubuntu':
-  command => 'chown -R ubuntu:ubuntu /data/',
-  require => File['/data'],
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School\n",
-  require => Package['nginx'],
-}
+  content => "Holberton School Nginx\n"
+} ->
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n",
-  require => Package['nginx'],
-}
+  content => "Ceci n'est pas une page\n"
+} ->
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $nginx_conf,
-  require => Package['nginx'],
-}
+  content => $nginx_conf
+} ->
 
-service {'nginx':
-  ensure => 'running',
-  enable => true,
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
